58a5211dd4dfd6f68bc093d755ffa70cf69037e2
Wbdgdz <dawei.zhou@etu.univ-poitiers.fr>
2025-11-25 17:05:19 +0100

    feat: ✨ add dashboard route

---
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -0,0 +1,10 @@
+function DashboardPage() {
+  return (
+    <section>
+      <h1>Dashboard</h1>
+      {/* Content will be implemented in next steps */}
+    </section>
+  );
+}
+
+export default DashboardPage;
